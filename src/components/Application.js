f51fd3f41b037a3e19b943fe27e5f0678f3303f0
John Webster <johnrwebs@gmail.com>
2019-09-03 15:43:33 -0600

    added axios, switched days to axious get

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment/index";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0
-  }
-];
 
 const appointments = [
   {
@@ -79,33 +62,40 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
 
-  const scheduleList = appointments.map((appointment) => {
-    return <Appointment key={appointment.id} {...appointment} />
+  useEffect(() => {
+    axios.get("/api/days").then(response => {
+      setDays(response.data);
+    });
+  }, []);
+
+  const scheduleList = appointments.map(appointment => {
+    return <Appointment key={appointment.id} {...appointment} />;
   });
 
-    return (
-      <main className="layout">
-        <section className="sidebar">
-          <img
-            className="sidebar--centered"
-            src="images/logo.png"
-            alt="Interview Scheduler"
-          />
-          <hr className="sidebar__separator sidebar--centered" />
-          <nav className="sidebar__menu">
-            <DayList days={days} day={day} setDay={setDay} />
-          </nav>
-          <img
-            className="sidebar__lhl sidebar--centered"
-            src="images/lhl.png"
-            alt="Lighthouse Labs"
-          />
-        </section>
-        <section className="schedule">
-          {scheduleList}
-          <Appointment key="last" time="5pm" />
-        </section>
-      </main>
-    );
-  };
+  return (
+    <main className="layout">
+      <section className="sidebar">
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList days={days} day={day} setDay={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
+      </section>
+      <section className="schedule">
+        {scheduleList}
+        <Appointment key="last" time="5pm" />
+      </section>
+    </main>
+  );
+}
